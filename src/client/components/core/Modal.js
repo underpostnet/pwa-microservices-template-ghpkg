3363b86e72a2f6e0870e91212a8641d8c760cad7
underpostnet <fcoverdugoa@underpost.net>
2024-11-02 16:38:37 +0000

    Update github repo package

--- src/client/components/core/Modal.js
+++ src/client/components/core/Modal.js
@@ -86,6 +86,7 @@ const Modal = {
       onDragEndListener: {},
       onObserverListener: {},
       onClickListener: {},
+      onExpandUiListener: {},
       query: options.query ? `${window.location.search}` : undefined,
     };
     if (options && 'mode' in options) {
@@ -296,6 +297,12 @@ const Modal = {
                 });
                 Responsive.Event[`view-${'main-body'}`]();
                 if (Responsive.Event[`view-${'bottom-bar'}`]) Responsive.Event[`view-${'bottom-bar'}`]();
+                if (Responsive.Event[`view-${'main-body-top'}`]) Responsive.Event[`view-${'main-body-top'}`]();
+                for (const keyEvent of Object.keys(this.Data[idModal].onExpandUiListener)) {
+                  this.Data[idModal].onExpandUiListener[keyEvent](
+                    !s(`.main-body-btn-ui-open`).classList.contains('hide'),
+                  );
+                }
               };
             });
 
@@ -1056,11 +1063,19 @@ const Modal = {
 
                 Responsive.Event[`view-${id}`] = () => {
                   if (!this.Data[id] || !s(`.${id}`)) return delete Responsive.Event[`view-${id}`];
-                  s(`.${id}`).style.height = `${
-                    window.innerHeight -
-                    (options.heightTopBar ? options.heightTopBar : heightDefaultTopBar) -
-                    (options.heightBottomBar ? options.heightBottomBar : heightDefaultBottomBar)
-                  }px`;
+                  s(`.${id}`).style.height = s(`.main-body-btn-ui-close`).classList.contains('hide')
+                    ? `${window.innerHeight}px`
+                    : `${
+                        window.innerHeight -
+                        (options.heightTopBar ? options.heightTopBar : heightDefaultTopBar) -
+                        (options.heightBottomBar ? options.heightBottomBar : heightDefaultBottomBar)
+                      }px`;
+
+                  if (s(`.main-body-btn-ui-close`).classList.contains('hide')) {
+                    s(`.${id}`).style.top = '0px';
+                  } else {
+                    s(`.${id}`).style.top = `${options.heightTopBar ? options.heightTopBar : heightDefaultTopBar}px`;
+                  }
                 };
                 Responsive.Event[`view-${id}`]();
 
