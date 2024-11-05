abdc39d66acf82c05e8ee43bb86e2ca4111c594f
underpostnet <fcoverdugoa@underpost.net>
2024-11-05 11:53:39 +0000

    Update github repo package

--- src/server/conf.js
+++ src/server/conf.js
@@ -838,6 +838,8 @@ const Cmd = {
   conf: (deployId, env) => `node bin/deploy conf ${deployId} ${env ? env : 'production'}`,
   replica: (deployId, host, path) => `node bin/deploy build-single-replica ${deployId} ${host} ${path}`,
   syncPorts: (deployGroupId) => `node bin/deploy sync-env-port ${deployGroupId}`,
+  cron: (deployId, job, expression) =>
+    `pm2 delete ${deployId}-${job} && env-cmd -f .env.production pm2 start bin/cron.js --no-autorestart --instances 1 --cron "${expression}" --name ${deployId}-${job} -- ${job} ${deployId}`,
 };
 
 const fixDependencies = async () => {
